55c7ad76094d90fd4b31ff365a7dc58754536321
Kayz <kayzcloud@hotmail.com>
2026-09-07 05:59:39 +0100

    Register required resource providers for azurerm v5

--- providers.tf
+++ providers.tf
@@ -4,11 +4,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 5.4.0"
+      version = "~> 5.4"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+
+  resource_providers_to_register = [
+    "Microsoft.Compute",
+    "Microsoft.Network",
+    "Microsoft.Storage",
+    "Microsoft.Insights",
+  ]
 }
